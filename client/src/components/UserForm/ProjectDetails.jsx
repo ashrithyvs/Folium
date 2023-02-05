4b7c4777bbe37a9cfb4d85e417e8075fd8b21d06
ashrithyvs <ashrithy20@gmail.com>
2023-02-05 09:14:55 +0530

    Removed ProjectONETWOTHREE

--- client/src/components/UserForm/ProjectDetails.jsx
+++ client/src/components/UserForm/ProjectDetails.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export const ProjectDetails = (props) => {
   const [projectsListItem, setprojectsListItem] = useState("");
-  const { state, handleChange, setState } = props;
+  const { state, setState } = props;
   const [projectsList, setprojectsList] = useState(state.projects);
   const cont = (e) => {
     e.preventDefault();
@@ -129,85 +129,7 @@ export const ProjectDetails = (props) => {
               return null;
             })}
         </div>
-        {/* <input
-          placeholder="Project One Title"
-          onChange={handleChange("projectOneTitle")}
-          defaultValue={state.projectOneTitle}
-          className="custom-input"
-        />
-        <input
-          placeholder="Project One Description"
-          onChange={handleChange("projectOneText")}
-          defaultValue={state.projectOneText}
-          className="custom-input"
-        />
-        <input
-          placeholder="Project One Url"
-          onChange={handleChange("projectOneUrl")}
-          defaultValue={state.projectOneUrl}
-          className="custom-input"
-        />
-        <input
-          placeholder="projectOne Image"
-          onChange={handleChange("projectOneImage")}
-          defaultValue={state.projectOneImage}
-          className="custom-input"
-        /> */}
       </div>
-
-      {/* <div className="flex flex-col space-y-2">
-        <input
-          placeholder="Project Two Title"
-          onChange={handleChange("projectTwoTitle")}
-          defaultValue={state.projectTwoTitle}
-          className="custom-input"
-        />
-        <input
-          placeholder="Project Two Description"
-          onChange={handleChange("projectTwoText")}
-          defaultValue={state.projectTwoText}
-          className="custom-input"
-        />
-        <input
-          placeholder="Project Two Url"
-          onChange={handleChange("projectTwoUrl")}
-          defaultValue={state.projectTwoUrl}
-          className="custom-input"
-        />
-        <input
-          placeholder="projectTwo Image"
-          onChange={handleChange("projectTwoImage")}
-          defaultValue={state.projectTwoImage}
-          className="custom-input"
-        />
-      </div>
-
-      <div className="flex flex-col space-y-2">
-        <input
-          placeholder="Project Three Title"
-          onChange={handleChange("projectThreeTitle")}
-          defaultValue={state.projectThreeTitle}
-          className="custom-input"
-        />
-        <input
-          placeholder="Project Three Description"
-          onChange={handleChange("projectThreeText")}
-          defaultValue={state.projectThreeText}
-          className="custom-input"
-        />
-        <input
-          placeholder="Project Three Url"
-          onChange={handleChange("projectThreeUrl")}
-          defaultValue={state.projectThreeUrl}
-          className="custom-input"
-        />
-        <input
-          placeholder="Project Three Image"
-          onChange={handleChange("projectThreeImage")}
-          defaultValue={state.projectThreeImage}
-          className="custom-input"
-        />
-      </div> */}
 
       <div className="flex justify-center space-x-4">
         <button className="custom-btn-outline max-w-min px-4" onClick={back}>
--- client/src/tOneComp/TempOneProjects.jsx
+++ client/src/tOneComp/TempOneProjects.jsx
@@ -27,51 +27,6 @@ export default function TempOneProjects({ templateData }) {
             </div>
           );
         })}
-      {/* <div className="bg-[#1b2232] container 2xl:w-1/2 md:px-8 xl:px-8 py-10 rounded-2xl flex flex-col items-center justify-center space-y-6">
-        <a
-          target="_blank"
-          className="bg-transparent text-white underline text-3xl "
-          href={templateData.projectOneUrl}
-          rel="noreferrer"
-        >
-          {templateData.projectOneTitle}
-        </a>
-        <img
-          alt={templateData.projectOneTitle}
-          src={templateData.projectOneImage}
-        />
-        <p className="bg-transparent"> {templateData.projectOneText} </p>
-      </div>
-      <div className="bg-[#1b2232] container 2xl:w-1/2 md:px-8 xl:px-8 py-10 rounded-2xl  flex flex-col items-center justify-center space-y-6">
-        <a
-          target="_blank"
-          className="bg-transparent text-white underline text-3xl "
-          href={templateData.projectTwoUrl}
-          rel="noreferrer"
-        >
-          {templateData.projectTwoTitle}
-        </a>
-        <img
-          alt={templateData.projectTwoTitle}
-          src={templateData.projectTwoImage}
-        />
-        <p className="bg-transparent"> {templateData.projectTwoText} </p>
-      </div>
-      <div className="bg-[#1b2232] container 2xl:w-1/2 md:px-8 xl:px-8 py-10 rounded-2xl  flex flex-col items-center justify-center space-y-6">
-        <a
-          target="_blank"
-          className="bg-transparent text-white underline text-3xl "
-          href={templateData.projectThreeUrl}
-          rel="noreferrer"
-        >
-          {templateData.projectThreeTitle}
-        </a>
-        <img
-          alt={templateData.projectThreeTitle}
-          src={templateData.projectThreeImage}
-        />
-        <p className="bg-transparent"> {templateData.projectThreeText} </p>
-      </div> */}
     </div>
   );
 }
